a5db8163a76f53ed2614df4afa1760498dd00847
Kody <kody.reid7@gmail.com>
2021-06-21 18:28:53 -0400

    hello world

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://kody:XmSSkTY7f9BjSACZ@cluster0.hnjsq.mongodb.net/books?retryWrites=true&w=majority"
+};
